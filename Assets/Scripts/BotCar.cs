9e20b9b5784588b42806c141750791ccebf46b8f
gnardini <gonzalo.nardini.dev@gmail.com>
2016-10-04 00:42:18 -0300

    The AI now completed the whole circuit

--- Assets/Scripts/BotCar.cs
+++ Assets/Scripts/BotCar.cs
@@ -11,21 +11,31 @@ public class BotCar : Car {
 		new Vector3(80f,2.3f,20.0f),
 		new Vector3(80f,2.3f,40.0f),
 		new Vector3(70f,2.3f,60.0f),
-		new Vector3(40f,2.3f,70.0f),
-		new Vector3(0f,2.3f,70.0f),
-		new Vector3(-10f,2.3f,90.0f),
-		new Vector3(-10f,2.3f,110.0f),
+		new Vector3(6f,2.3f,65.0f),
+		new Vector3(-7f,2.3f,77.0f),
+		new Vector3(-7f,2.3f,94.0f),
+		new Vector3(3f,2.3f,115.0f),
+        new Vector3(73f,2.3f,120.0f),
+        new Vector3(87f,2.3f,140.0f),
+        new Vector3(86f,2.3f,154.0f),
+        new Vector3(81f,2.3f,170.0f),
+        new Vector3(59f,2.3f,180.0f),
+        new Vector3(-58f,2.3f,177.0f),
+        new Vector3(-68f,2.3f,169.0f),
+        new Vector3(-77f,2.3f,161.0f),
+        new Vector3(-74f,2.3f,21.0f),
+        new Vector3(-65f,2.3f,9.0f),
+        new Vector3(-52f,2.3f,3.0f),
 	};
 	private int i = 0;
 
 	void Update(){
-        if (checkPaused() || index >= points.Length) {
+        if (checkPaused()) {
 			return;
 		}
-		//Debug.Log (transform.position);
 		Vector3 sub = points [index] - transform.position;
 		if (sub.magnitude < 5) {
-			index = (index + 1);
+            index = (index + 1) % points.Length;
 		} else {
 			Vector3 dir = -transform.forward;
 			double vectorProd = sub.x * dir.z - sub.z * dir.x;
