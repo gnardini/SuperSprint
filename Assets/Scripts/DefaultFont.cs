4b284c1c0f22f8daf1108ed7814d9589073b0a41
gnardini <gonzalo.nardini.dev@gmail.com>
2016-10-05 10:41:52 -0300

    Adds Robot font

---
+++ Assets/Scripts/DefaultFont.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DefaultFont : MonoBehaviour {
+
+	void Start () {
+        Font defaultFont = Resources.Load("Roboto-Medium") as Font;
+        foreach (Text text in GetComponentsInChildren<Text>()) {
+            text.font = defaultFont;
+        }
+	}
+
+}
